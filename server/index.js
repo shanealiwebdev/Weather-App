0522dcb8810a3dd7f869c1ca04bc244fcd64a66a
Shan-E-Ali <shanealitiger@gmail.com>
2026-07-16 15:41:44 +0500

    Mongodb removed

--- server/index.js
+++ server/index.js
@@ -1,25 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const dns = require("dns");
 const weatherRoutes = require("./weatherRoutes");
 const cors = require("cors");
 require("dotenv").config();
 
-dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 const app = express();
 app.use(express.json());
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-    }
-}
-connectDB();
 app.use(cors())
 app.use("/api/weather", weatherRoutes);
 const PORT = process.env.PORT || 5000;
